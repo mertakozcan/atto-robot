9592487b50aecb6f63e5cda1b3c54dc9fa1b47e5
Mert <mertakozcan@gmail.com>
2017-12-09 03:01:46 -0800

    Implement data transfer between app and board

--- Atto/app/src/main/java/com/example/mert/atto/MainActivity.java
+++ Atto/app/src/main/java/com/example/mert/atto/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.mert.atto;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -26,19 +25,39 @@ import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
 
-    public static final int MESSAGE_READ = 0;
-    public static final int MESSAGE_WRITE = 1;
-    public static final int MESSAGE_TOAST = 2;
+    private static final String DEVICE_NAME = "JerryCar";
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
-    public static final int REQUEST_ENABLE_BT = 1;
-    public static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-    // alternative, 00000000-0000-1000-8000-00805f9b34fb
+    private static final int MESSAGE_READ = 0;
+    private static final int MESSAGE_WRITE = 1;
+    private static final int MESSAGE_TOAST = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
 
-    Button leftButton, rightButton, backButton;
+    Button leftButton, rightButton, backButton, autoButton, manualButton;
     TextView feedbackScreen;
     BluetoothAdapter mBluetoothAdapter;
     ConnectThread connectThread;
     ConnectedThread connectedThread;
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    Log.d("WRITE", writeMessage);
+                    break;
+                case MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    feedbackScreen.setText(readMessage);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +68,8 @@ public class MainActivity extends Activity {
         leftButton      = (Button)findViewById(R.id.leftButton);
         rightButton     = (Button)findViewById(R.id.rightButton);
         backButton      = (Button)findViewById(R.id.backButton);
+        autoButton      = (Button)findViewById(R.id.autoButton);
+        manualButton    = (Button)findViewById(R.id.manualButton);
         feedbackScreen  = (TextView)findViewById(R.id.feedbackScreen);
 
         // Initialize Bluetooth adapter
@@ -67,27 +88,62 @@ public class MainActivity extends Activity {
         if (pairedDevices.size() > 0) {
             for (BluetoothDevice device : pairedDevices) {
                 String deviceName = device.getName();
-                // String deviceHardwareAddress = device.getAddress(); // MAC address
-                if (deviceName.contains("Jerry")) {
+                if (deviceName.contains(DEVICE_NAME)) {
                     connectThread = new ConnectThread(device);
+                    connectThread.run();
                     break;
                 }
             }
         }
-        if (connectThread != null) {
-            connectThread.run();
+    }
+
+    public void selectAutomatic(View view) {
+        try {
+            connectedThread.write("k");
+        } catch (Exception e){
+            Log.d("ERROR", "NULL_THREAD");
+        }
+    }
+
+    public void selectManual(View view) {
+        try {
+            connectedThread.write("l");
+        } catch (Exception e){
+            Log.d("ERROR", "NULL_THREAD");
+        }
+    }
+
+    public void turnLeft(View view) {
+        try {
+            connectedThread.write("a");
+        } catch (Exception e){
+            Log.d("ERROR", "NULL_THREAD");
+        }
+    }
+
+    public void turnRight(View view) {
+        try {
+            connectedThread.write("d");
+        } catch (Exception e){
+            Log.d("ERROR", "NULL_THREAD");
+        }
+    }
+
+    public void turnBack(View view) {
+        try {
+            connectedThread.write("s");
+        } catch (Exception e){
+            Log.d("ERROR", "NULL_THREAD");
         }
     }
 
     private class ConnectThread extends Thread {
         private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
 
-        public ConnectThread(BluetoothDevice device) {
+        private ConnectThread(BluetoothDevice device) {
             // Use a temporary object that is later assigned to mmSocket
             // because mmSocket is final.
             BluetoothSocket tmp = null;
-            mmDevice = device;
 
             try {
                 // Get a BluetoothSocket to connect with the given BluetoothDevice.
@@ -99,6 +155,7 @@ public class MainActivity extends Activity {
             mmSocket = tmp;
         }
 
+        @Override
         public void run() {
             // Cancel discovery because it otherwise slows down the connection.
             mBluetoothAdapter.cancelDiscovery();
@@ -119,14 +176,13 @@ public class MainActivity extends Activity {
 
             // The connection attempt succeeded. Perform work associated with
             // the connection in a separate thread.
-            manageMyConnectedSocket(mmSocket);
-        }
-
-        public void manageMyConnectedSocket(BluetoothSocket mmSocket) {
-            connectedThread = new ConnectedThread(mmSocket);
-            connectedThread.write("k");
-            connectedThread.write("a");
-            connectedThread.write("d");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    connectedThread = new ConnectedThread(mmSocket);
+                    connectedThread.run();
+                }
+            }).start();
         }
 
         // Closes the client socket and causes the thread to finish.
@@ -144,13 +200,11 @@ public class MainActivity extends Activity {
         private final InputStream mmInStream;
         private final OutputStream mmOutStream;
         private byte[] mmBuffer; // mmBuffer store for the stream
-        private Handler mHandler;
 
-        public ConnectedThread(BluetoothSocket socket) {
+        private ConnectedThread(BluetoothSocket socket) {
             mmSocket = socket;
             InputStream tmpIn = null;
             OutputStream tmpOut = null;
-            mHandler = new Handler();
             // Get the input and output streams; using temp objects because
             // member streams are final.
             try {
@@ -168,6 +222,7 @@ public class MainActivity extends Activity {
             mmOutStream = tmpOut;
         }
 
+        @Override
         public void run() {
             mmBuffer = new byte[1024];
             int numBytes; // bytes returned from read()
@@ -182,7 +237,6 @@ public class MainActivity extends Activity {
                             MESSAGE_READ, numBytes, -1,
                             mmBuffer);
                     readMsg.sendToTarget();
-                    Log.d("READ_MSG", readMsg.toString());
                 } catch (IOException e) {
                     Log.d(TAG, "Input stream was disconnected", e);
                     break;
@@ -191,17 +245,14 @@ public class MainActivity extends Activity {
         }
 
         // Call this from the main activity to send data to the remote device.
-        public void write(String message) {
+        private void write(String message) {
             try {
-                Log.d("Message", message);
                 byte[] bytes = message.getBytes();
                 mmOutStream.write(bytes);
-                /*
                 // Share the sent message with the UI activity.
                 Message writtenMsg = mHandler.obtainMessage(
                         MESSAGE_WRITE, -1, -1, mmBuffer);
                 writtenMsg.sendToTarget();
-                */
             } catch (IOException e) {
                 Log.e(TAG, "Error occurred when sending data", e);
 
